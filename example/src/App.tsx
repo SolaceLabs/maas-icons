4b86a5791b3fa174c46d0d26a0c67d3580d59552
dipin-anand <dipin.anand@solace.com>
2022-01-28 17:16:13 -0500

    create a npm package for maas-icons

---
+++ example/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { ReactComponent as YourSvg } from "maas-icons/dist/svg/sprite.symbol.svg";
+
+
+const App = () => {
+  
+  return (
+    <div>
+      <div style={{ display: "none" }}>
+        <YourSvg />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/icons.ts
@@ -0,0 +1,4 @@
+export enum Icons {
+  Add_24 = "icons_24px_Add",
+  Announcement_24 = "icons_24px_Announcement",
+}
