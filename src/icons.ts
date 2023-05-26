ca61949014745a05486b4e8406d79d9f6a879924
Steve Thomas <steven_thomas_12@hotmail.com>
2023-05-26 18:21:43 -0400

    PATCH: ran build to execute the build:svg script

--- src/icons.ts
+++ src/icons.ts
@@ -2,8 +2,11 @@ export enum Icons {
   add_24 = "icons_24px_add",
   announcement_24 = "icons_24px_announcement",
   back_24 = "icons_24px_back",
+  calendar_24 = "icons_24px_calendar",
   close_24 = "icons_24px_close",
   delete_24 = "icons_24px_delete",
   filter_24 = "icons_24px_filter",
-  search_24 = "icons_24px_search"
+  psCloudLogo_24 = "icons_24px_psCloudLogo",
+  search_24 = "icons_24px_search",
+  usageReports_24 = "icons_24px_usageReports",
 }
